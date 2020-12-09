bbd248a23b832d6bd6897e7adc7efbbc8dcfd912
Maurice Alves <maurice.estevam@gmail.com>
2020-12-09 11:05:45 -0300

    Exercícios 3 e 4. dia 8.2.

--- bloco_8_HOF/dia_8.1/exercicio5.js
+++ bloco_8_HOF/dia_8.1/exercicio5.js
@@ -1,4 +1,4 @@
-const assert = require('assert');
+/*const assert = require('assert');
 
 const books = [
     {
@@ -72,4 +72,40 @@ function everyoneWasBornOnSecXX() {
     })
 }
 //console.log(everyoneWasBornOnSecXX());
-assert.equal(everyoneWasBornOnSecXX(), expectedResult);
+assert.equal(everyoneWasBornOnSecXX(), expectedResult);*/
+
+/*const numbers = [19, 21, 30, 3, 45, 22, 15];
+
+const verifyEven = (number) => number % 2 === 0;
+
+const isEven = numbers.filter(verifyEven);
+
+console.log(isEven); // [ 30, 22 ]
+
+// Outra forma de ser realizada sem a necessidade de criar uma nova função.
+const isEven2 = numbers.filter((number) => number % 2 === 0);
+
+console.log(isEven2); // [ 30, 22 ]*/
+
+/*const objPeople = [
+    { name: 'José', age: 21 },
+    { name: 'Lucas', age: 19 },
+    { name: 'Maria', age: 16 },
+    { name: 'Gilberto', age: 18 },
+    { name: 'Vitor', age: 15 },
+];
+
+const verifyAgeDrive = (arrayOfPeople) => (
+    arrayOfPeople.filter((people) => (people.age < 18))
+);
+
+console.log(verifyAgeDrive(objPeople)); // [ { name: 'Maria', age: 16 }, { name: 'Vitor', age: 15 } ]*/
+
+const arrayMyStudents = ['Maria', 'Manuela', 'Jorge', 'Ricardo', 'Wilson'];
+
+const removeStudentByName = (name, listStudents) => {
+    return listStudents.filter((student) => student !== name); // Filtra todos os estudantes que não têm o nome 'Ricardo' e retorna um array com eles. Na prática, remove o Ricardo do array.
+};
+
+const newListStudents = removeStudentByName('Ricardo', arrayMyStudents);
+console.log(newListStudents); // [ 'Maria', 'Manuela', 'Jorge', 'Wilson' ]
